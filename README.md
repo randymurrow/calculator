ea6d12bd8c0f9324a508ec84fe5c0fda8fdc2801
Randy Murrow <randallmurrow@gmail.com>
2025-08-29 20:43:27 -0500

    Add event listeners to update display, select operator, and store initial operand

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Javascript Calculator
-An on-screen calculator built with JavaScript, HTML, and CSS as part of The Odin Project Foundation course: https://www.theodinproject.com/lessons/foundations-calculator
+An on-screen calculator built with JavaScript, HTML, and CSS as part of The Odin Project Foundations course: https://www.theodinproject.com/lessons/foundations-calculator
--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-function add(num1, num2) {
-    return num1 + num2;
-}
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => a / b;
 
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
-
-function operate(operator, num1, num2) {
-    return operator(num1, num2);
+function operate(operator, a, b) {
+    return operator(a, b);
 }
 
 // Create the functions that populate the display when you click the digit buttons
+document.addEventListener("DOMContentLoaded", () => {
+    let display = [];
+    let displayDigits = document.querySelector("#displayDigits");
+    let digits = document.querySelectorAll(".digit");
+
+    digits.forEach(button => {
+        button.addEventListener("click", () => {
+            display.push(button.textContent);
+            displayDigits.textContent = display.join("");
+        });
+    });
+    
 // Store the content of the display in a variable for use in the next step
+    let operands = [];
+    let operator;
+    let operators = document.querySelectorAll(".operator");
+
+    operators.forEach(button => {
+        button.addEventListener("click", () => {
+            operator = button.value;
+            operands.push(parseInt(displayDigits.textContent));
+        });
+    });
+});
 
 // Make the calculator work! Store the first and second numbers input by the user
 // Operate() on them when the user presses the = button, according to the operator selected
